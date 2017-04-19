5044c0e9d195389f47c4758339e1787798350036
rdsly199 <richie0594@hotmail.com>
2017-04-18 23:02:27 -0400

    BackgoundSprites
    Added the Background images for CPLevel and Tutorial Level

--- SplitScreenTest/Assets/Scripts/Finished.cs
+++ SplitScreenTest/Assets/Scripts/Finished.cs
@@ -7,14 +7,24 @@ public class Finished : MonoBehaviour {
 
     public Text GUIReady1;
     public Text GUIReady2;
+    public Text GUIText;
+    private bool reset;
 
     // Use this for initialization
     void Start () {
-		
+        reset = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+        if(reset == true)
+        {
+            if(Input.GetKey(KeyCode.R))
+            {
+                Application.LoadLevel("CPlevel");
+            }
+        }
 		
 	}
 
@@ -23,19 +33,22 @@ public class Finished : MonoBehaviour {
         if(other.gameObject.tag == "Player" || other.gameObject.tag == "Player2")
         {
             Time.timeScale = 0;
-            ReadySetGo.Set1 = false;
-            ReadySetGo.Set2 = false;
+            
 
             if (other.gameObject.tag == "Player")
             {
                 GUIReady1.text = "Player 1 Win";
                 GUIReady2.text = "Player 2 Lose";
+                GUIText.text = "Please press R to Restart";
+                reset = true;
             }
 
             if (other.gameObject.tag == "Player2")
             {
                 GUIReady1.text = "Player 1 Lose";
                 GUIReady2.text = "Player 2 Win";
+                GUIText.text = "Please press R to Restart";
+                reset = true;
             }
 
 
