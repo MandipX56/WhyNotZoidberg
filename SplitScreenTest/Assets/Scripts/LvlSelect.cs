ae23b8e2148d0aed406fd48c94ef847978351521
MandipX56 <mandipX56@gmail.com>
2017-04-20 13:35:30 -0400

    Added a few menus,and sprites

---
+++ SplitScreenTest/Assets/Scripts/LvlSelect.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LvlSelect : MonoBehaviour
+{
+    public string lvlSelect; 
+
+    //public void LoadScene()
+    //{
+    //    SceneManager.LoadScene(lvlSelect);
+    //}
+
+    public void OnButtonClick(string lvlSelect)
+    {
+        Application.LoadLevel(lvlSelect);
+    }
+
+}
