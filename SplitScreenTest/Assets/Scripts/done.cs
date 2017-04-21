dd1ab978d40211eb9e2687be9cd8509d2df22ae9
rdsly199 <Tech@C401IT11.gbc.local>
2017-04-21 11:13:14 -0400

    final

---
+++ SplitScreenTest/Assets/Scripts/done.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class done : MonoBehaviour {
+
+    public Text GUIReady1;
+
+   // public Text GUIText;
+    private bool reset;
+
+    // Use this for initialization
+    void Start()
+    {
+        reset = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (reset == true)
+        {
+            if (Input.GetKey(KeyCode.R))
+            {
+                Application.LoadLevel("CPlevel");
+            }
+
+            if (Input.GetKey(KeyCode.M))
+            {
+                Application.LoadLevel("MAIN MENU");
+            }
+
+        }
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player" || other.gameObject.tag == "Player2")
+        {
+            Time.timeScale = 0;
+
+
+            if (other.gameObject.tag == "Player")
+            {
+                GUIReady1.text = "Player 1 Win";
+
+                //GUIText.text = "Please press R to Restart or M to MainMenu";
+                reset = true;
+            }
+
+
+
+        }
+    }
+}
